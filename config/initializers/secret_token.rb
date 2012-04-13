f74d1946bddbfa21d92384d4a630af607fbd305a
Zachary Nicoll <zacharynicoll@gmail.com>
2012-04-12 20:59:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drdannicoll::Application.config.secret_token = 'c480ac5f33906f0595ccad48c3415d29b75b3952d55a81e05a51501f0f7309c3005061d67d0bd659066a31e59d9d9a8faa4ac7a6f97fad79a24c7d00509b355c'
