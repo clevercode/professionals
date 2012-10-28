49ac22ad5d0d76b7f730ad0a2c1247bd63cebac7
Zachary Nicoll <tower@zdn.me>
2012-10-28 17:51:40 -0400

    Modified pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
 
   def home
+    @title = 'Home'
+    @body_class = 'home'
+    @answers = Answer.limit(6)
+  end
+
+  def about
+    @title = 'About'
+    @body_class = 'about'
+  end
+
+  def contact
+    @title = 'Contact'
+    @body_class = 'contact'
   end
 end
 
