cb37e79647a2a0cb9f7e1a7eafd8610a68934103
Zachary Nicoll <tower@zdn.me>
2013-02-10 21:53:57 -0500

    Buggers.

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,12 @@ Drdannicoll::Application.routes.draw do
   get '/about' => 'pages#about'
 
   namespace :admin do
-    get  '/admin' => 'sessions#new', as: 'sign_in'
     get  'sign-in' => 'sessions#new', as: 'sign_in'
     post 'sign-in' => 'sessions#create'
     get  'sign-out' => 'sessions#destroy', as: 'sign_out'
 
-    root :to => 'pages#home'
+    root :to => 'answers#index'
 
-    resources :pages, only: [:home, :about], module: nil
     resources :questions, :articles, :answers, module: nil
   end
 
